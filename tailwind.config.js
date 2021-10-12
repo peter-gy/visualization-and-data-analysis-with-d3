b506e0b31ee83c5940dcb9c3de106b51baae0bb8
Péter Ferenc Gyarmati <dev.petergy@gmail.com>
2021-10-12 14:44:22 +0200

    play with alpine

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,18 @@
+const colors = require('tailwindcss/colors');
+
 module.exports = {
-  mode: "jit",
-  purge: ["*/**/*.html"],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+    mode: "jit",
+    purge: ["*.html", "*/**/*.html"],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {
+            colors: {
+                primary: colors.blue[600]
+            }
+        },
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
 };
